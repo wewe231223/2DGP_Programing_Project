227b33dee9f65969c04b24b8e31c3c8c9f034206
승범 김 <wewe231223@kpu.ac.kr>
2023-10-26 18:38:54 +0900

    캐릭터 스프라이트 추가

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ from pico2d import *
 
 open_canvas()
 
-
 character = load_image("Resources/Sprite.png")
-
-
 
 while True:
     clear_canvas()
 
-    character.draw(10,10)
+    character.draw(10, 10)
 
     update_canvas()

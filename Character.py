53fbaa856273a1d1ad955fa67c1ca3b0dab8f392
승범 김 <wewe231223@kpu.ac.kr>
2023-10-28 01:37:01 +0900

    캐릭터 애니메이션

--- Character.py
+++ Character.py
@@ -9,11 +9,15 @@ class Character:
         self.height = 150
         self.x = 100
         self.y = 100
+        self.action = 6
+        self.frame = 0
 
     def render(self):
-        self.image.clip_draw(0,self.width,self.width,self.width,self.x,self.y)
+        self.image.clip_draw(self.frame * self.width, self.width * self.action, self.width, self.width, self.x, self.y,300,300)
 
 
     def update(self):
+        self.frame = (self.frame+1) % 5
+        delay(0.3)
         pass
 

7ac2850d8709352f8e6d2cbadcb68caac5ffe14b
승범 김 <wewe231223@kpu.ac.kr>
2023-10-27 21:14:47 +0900

    캐릭터 스프라이트 변경

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import pico2d
 from pico2d import *
 
 open_canvas()
 
-character = load_image("Resources/Sprite.png")
+character = load_image("Resources/character_animations.png")
+
+
+
+def handleevent(events):
+    for event in events:
+        if event.type == SDL_QUIT:
+            exit(0)
+        if event.type ==SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            exit(0)
+
+Eventlist = []
 
 while True:
     clear_canvas()
 
-    character.draw(10, 10)
+    character.clip_draw(0,150,150,150,400,400);
+
+    Eventlist = get_events()
+
+    handleevent(Eventlist)
 
     update_canvas()
